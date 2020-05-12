86256217ede194c75f072e9d9d9c49625edecb58
Frisys <frisys@Frisys.local>
2020-05-12 10:22:27 +0900

    チュートリアル５終了

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,17 @@ module.exports = {
   plugins: [
     `gatsby-plugin-emotion`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
     },
   ],
-}
+};
