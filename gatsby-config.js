874f80588d6d9e8a606b26b0663ff09f7147daac
Frisys <frisys@Frisys.local>
2020-05-12 12:10:03 +0900

    チュートリアル８終了

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
 module.exports = {
   siteMetadata: {
     title: "Pandas Eating Lots",
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`, 
   },
   plugins: [
     `gatsby-plugin-emotion`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -16,6 +19,18 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
       },
     },
   ],
